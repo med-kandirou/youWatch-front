8652613ac108c8eacffc544cb0a8af37aab0724c
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2024-03-11 14:48:46 +0000

    update

--- src/pages/videoPage.tsx
+++ src/pages/videoPage.tsx
@@ -89,7 +89,6 @@ function VideoPage () {
                             </svg>
                             <span>798</span>
                         </button>
-                        
                         <button className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex items-center space-x-1">
                             <svg
                             xmlns="http://www.w3.org/2000/svg"
