92bb4e419f0273285c8dee59b465fa5abb18f7d4
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2024-03-17 14:29:44 +0000

    add system to upload file

--- src/uploadFiles/app.py
+++ src/uploadFiles/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS_AUDIO = {'mp3'}
-ALLOWED_EXTENSIONS_IMAGE = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS_VIDEO = {'mp4'}
+ALLOWED_EXTENSIONS_IMAGE = {'png', 'jpg', 'jpeg','jfif', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -16,18 +16,18 @@ def allowed_file(filename, allowed_extensions):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
-@app.route('/upload-audio', methods=['POST'])
-def upload_audio():
+@app.route('/upload-video', methods=['POST'])
+def upload_video():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
-    if file and allowed_file(file.filename, ALLOWED_EXTENSIONS_AUDIO):
+    if file and allowed_file(file.filename, ALLOWED_EXTENSIONS_VIDEO):
         filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]  # UUID with original extension
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return jsonify({'url': f"{request.url_root}{UPLOAD_FOLDER}/{filename}"})
-    return jsonify({'error': 'File type not allowed for audio'})
+    return jsonify({'error': 'File type not allowed for video'})
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
@@ -42,7 +42,7 @@ def upload_image():
         return jsonify({'url': f"{request.url_root}{UPLOAD_FOLDER}/{filename}"})
     return jsonify({'error': 'File type not allowed for image'})
 
-def generate_audio(filepath):
+def generate_video(filepath):
     CHUNK = 1024
     with open(filepath, 'rb') as f:
         while True:
@@ -52,9 +52,9 @@ def generate_audio(filepath):
             yield data
 
 @app.route('/uploads/<filename>')
-def audio_stream(filename):
+def video_stream(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    return Response(generate_audio(filepath), mimetype="audio/*")
+    return Response(generate_video(filepath), mimetype="video/*")
 
 if __name__ == '__main__':
     app.run(debug=True)
